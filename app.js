5ff77ef60106c6e03418fbad4036b383654416f3
Nikhil Jha <nikhiljhanov15@gmail.com>
2022-12-30 15:30:19 +0530

    styling and footer

--- app.js
+++ app.js
@@ -2,8 +2,25 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#outputdiv")
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text){
+    return serverURL + "?text="+ text;
+}
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("Something wrong with server! try again later")
+}
+
 function clickHandler() {
-   outputDiv.innerText = "result "+txtInput.value;
+ 
+   var inputText= txtInput.value;
+   var URL= getTranslationURL(inputText);
+   fetch(URL)
+     .then(response => response.json())
+     .then(json => outputDiv.innerText=json.contents.translated)
+     .catch(errorHandler)
 }
 
 btnTranslate.addEventListener("click", clickHandler)
